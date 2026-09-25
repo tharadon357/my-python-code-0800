2e6aca95da6d886ebc104d25e17ea4e7e3c6cd12
Tharadon <aoawzaa@gmail.com>
2026-09-25 13:51:32 +0700

    ทำแล้ว

--- assignments/week07/quiz-1.py
+++ assignments/week07/quiz-1.py
@@ -6,19 +6,18 @@
 """
 
 class Rectangle:
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
-
-    # Method to get the area
-    def get_area(self):
-        pass
-
-    # Method to get the perimeter
-    def get_perimeter(self):
-        pass
-
-
+   def __init__(self, length, width):
+       self.length = length
+       self.width = width
+   # Method to get the area
+   def get_area(self):
+       pass
+       return self.length * self.width #กว้าง * ยาว
+   # Method to get the perimeter
+   def get_perimeter(self):
+       pass
+       return 2 * (self.length + self.width)
+ 
 rect = Rectangle(10, 5)
 print(rect.get_area())       # Should print 50
 print(rect.get_perimeter())  # Should print 30
